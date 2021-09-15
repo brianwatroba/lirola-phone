acfd186a9afc579ec51a7b81186e0b991a06bc73
Brian Watroba <brianwatroba@gmail.com>
2021-09-15 10:50:27 -0400

    screen fits, need to fix responsive fonts and icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Phone from './Phone';
+
 import Screen from './Screen';
 
 const Main = () => {
@@ -14,15 +15,21 @@ const Main = () => {
 };
 
 const FlexContainer = styled.div`
-	position: absolute;
-	top: 0;
 	display: flex;
-	align-items: center;
-	flex-direction: column;
 	text-align: center;
-	height: 100vh;
+	height: 100%;
 	width: 100%;
 	background-color: #222;
 `;
+
+// const Screen = styled.div`
+// 	background-color: green;
+// 	z-index: 10;
+// 	position: absolute;
+// 	width: 80%;
+// 	height: 225.823px;
+// 	top: 10%;
+// 	left: 43.9%;
+// `;
 
 export default Main;
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { keyframes, css } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import phoneWallpaper from '../icons/phonewallpaper.png';
 import batteryIcon from '../icons/battery.png';
 import signalIcon from '../icons/signal.png';
