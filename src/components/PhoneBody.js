e2cd42c152dc56282bda4d251b50bf09463535aa
Brian Watroba <brianwatroba@gmail.com>
2021-10-07 09:28:10 -0400

    sound works and fixed bar

--- src/components/PhoneBody.js
+++ src/components/PhoneBody.js
@@ -5,6 +5,13 @@ import './phone.css';
 import getPdfUrl from '../utils/getPdfUrl';
 import takePhoto from '../utils/takePhoto';
 
+import keytone1 from '../sounds/keytone1.mp3';
+import keytone2 from '../sounds/keytone2.mp3';
+import keytone3 from '../sounds/keytone3.mp3';
+import dialing from '../sounds/dialing.mp3';
+import invalidnumber from '../sounds/invalidnumber.mp3';
+import camerashutter from '../sounds/camerashutter.mp3';
+
 const PhoneBody = ({
 	isOpen,
 	setIsOpen,
@@ -22,8 +29,64 @@ const PhoneBody = ({
 	videoContainerRef,
 	photoRef,
 }) => {
+	const keyTone1 = new Audio(keytone1);
+	const keyTone2 = new Audio(keytone2);
+	const keyTone3 = new Audio(keytone3);
+	const dialTone = new Audio(dialing);
+	const invalidNumber = new Audio(invalidnumber);
+	const cameraShutter = new Audio(camerashutter);
+
+	const playSound = (key) => {
+		switch (key) {
+			case 'takePhoto':
+				cameraShutter.play();
+				break;
+			case 'invalid':
+				dialTone.pause();
+				invalidNumber.play();
+				break;
+			case 'dialing':
+				dialTone.play();
+				break;
+			case 'website':
+				keyTone1.play();
+				break;
+			case 'instagram':
+				keyTone2.play();
+				break;
+			case 'email':
+				keyTone3.play();
+				break;
+			case 'ok':
+				keyTone1.play();
+				break;
+			case 'camera':
+				keyTone2.play();
+				break;
+			case 'back':
+				keyTone3.play();
+				break;
+			case 'cancel':
+				keyTone3.play();
+				break;
+			case '0':
+				keyTone1.play();
+				break;
+			case '1':
+				keyTone2.play();
+				break;
+			case '2':
+				keyTone3.play();
+				break;
+			default:
+				keyTone1.play();
+				return;
+		}
+	};
+
 	const handleNumKeyPress = (e) => {
 		const value = e.target.id;
+		playSound(value);
 		if (!isOpen.input && !isOpen.sending) {
 			setIsOpen({ ...isOpen, input: true });
 			setEntered((oldArray) => [...oldArray, value]);
@@ -33,6 +96,7 @@ const PhoneBody = ({
 	};
 
 	const handleCancelPress = async () => {
+		playSound('cancel');
 		setLoading(false);
 		setEntered([]);
 		setHasPhoto(false);
@@ -49,14 +113,17 @@ const PhoneBody = ({
 	};
 
 	const handleOkPress = async () => {
+		playSound('ok');
 		if (isOpen.input) {
 			try {
 				setLoading(true);
 				setIsOpen({ ...isOpen, input: false });
 				setMessages({ ...messages, loading: 'DIALING' });
+				playSound('dialing');
 				const url = await getPdfUrl(entered.join(''));
 				redirect(url);
 			} catch (error) {
+				playSound('invalid');
 				setLoading(false);
 				setIsOpen({ ...isOpen, input: false });
 				setEntered([]);
@@ -77,15 +144,17 @@ const PhoneBody = ({
 	};
 
 	const handleBackspacePress = () => {
+		playSound('back');
 		if (entered.length > 0) {
 			setEntered((oldArray) => oldArray.slice(0, -1));
 		} else {
-			setInputOpen(false);
+			setIsOpen({ ...isOpen, input: false });
 		}
 	};
 
 	const handleLinkPress = (e) => {
 		const destination = e.target.id;
+		playSound(destination);
 		const confirmed = (msg) => {
 			const ok = window.confirm(msg);
 			return ok;
@@ -112,13 +181,18 @@ const PhoneBody = ({
 	const handleCameraPress = () => {
 		if (isOpen.camera) {
 			if (hasPhoto) {
+				playSound('camera');
 				setHasPhoto(false);
 			} else {
+				playSound('takePhoto');
 				setHasPhoto(true);
 				takePhoto(videoRef, photoRef, videoContainerRef);
 			}
 		}
-		if (!isOpen.camera) setIsOpen({ ...isOpen, camera: !isOpen.camera });
+		if (!isOpen.camera) {
+			playSound('camera');
+			setIsOpen({ ...isOpen, camera: !isOpen.camera });
+		}
 	};
 
 	const redirect = (url) => {
